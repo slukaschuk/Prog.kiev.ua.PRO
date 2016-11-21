312ef1b7d6f8f80969c54e521b61a36f95871f29
Sergey Lukaschuk <sergey.lukaschuk@gmail.com>
2016-11-21 23:01:48 +0200

    add comments

--- Annotationhometask1/TestSaver.java
+++ Annotationhometask1/TestSaver.java
@@ -1,6 +1,12 @@
 package Annotationhometask1;
 
 import java.lang.reflect.*;
+/**
+ * Write TextContainer class that contains a string. With annotation mechanism to specify
+ 1) in which the file should be preserved text
+ 2) method, which performs preservation. Write class Saver,
+ which will keep the field TextContainer class to the specified file.
+ */
 
 public class TestSaver {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
--- Annotationhometask2/TestSave.java
+++ Annotationhometask2/TestSave.java
@@ -3,7 +3,10 @@ package Annotationhometask2;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
+/*
+* Write code that is serialized and deserializes into / from a file all the values of a class of fields that
+noted annotation @Save.
+*/
 public class TestSave {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchFieldException {
         TextContainer tc = new TextContainer("String", 200, true, "Not Saved");
